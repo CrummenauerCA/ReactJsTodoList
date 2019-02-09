8ca8bb96323fd9f10f73b4af9d29d99cd76e9d59
Cezar Augusto Crummenauer <cezar.augusto.web@gmail.com>
2019-02-09 03:10:09 -0200

    Exibindo informações de estado através de exemplos

--- src/App.js
+++ src/App.js
@@ -4,11 +4,33 @@ import './App.css';
 import Note from './note/Note';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      notes: [
+        { id: 1, noteContent: 'Nota 1 aqui!' },
+        { id: 2, noteContent: 'Nota 2 aqui!' },
+      ],
+    }
+  }
   render() {
     return (
-      <div>
-        <h1>React and Firebase To-Do List</h1>
-        <Note />
+      <div className="notesWrapper">
+        <div className="notesHeader">
+          <h1 className="heading">React and Firebase To-Do List</h1>
+        </div>
+        <div className="notesBody">
+          {
+            this.state.notes.map(note => {
+              return (
+                <Note noteContent={note.noteContent} noteId={note.noteId} key={note.id} />
+              );
+            })
+          }
+        </div>
+        <div className="notesFooter">
+          Rodapé será aqui...
+        </div>
       </div>
     );
   }
--- src/note/Note.jsx
+++ src/note/Note.jsx
@@ -1,24 +1,25 @@
 import React, { Component } from 'react';
-import  './Note.css';
+import './Note.css';
 import PropTypes from 'prop-types';
 
 class Note extends Component {
   constructor(props) {
     super(props);
-    this.message = 'Note component...';
+    this.noteId = props.noteId;
+    this.noteContent = props.noteContent;
   }
 
   render(props) {
     return (
-      <div>
-        <h1>{this.message}</h1>
+      <div className="note fade-in">
+        <p className="noteContent">{this.noteContent}</p>
       </div>
     )
   }
 }
 
 Note.propTypes = {
-
+  noteContent: PropTypes.string
 }
 
 export default Note;
