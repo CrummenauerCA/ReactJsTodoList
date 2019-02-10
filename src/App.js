f3cb2c5ac02e15df78b6eb8eeb714619195ba1b9
Cezar Augusto Crummenauer <cezar.augusto.web@gmail.com>
2019-02-10 02:31:48 -0200

    Enviando dados para o Firebase RT Database

--- src/App.js
+++ src/App.js
@@ -1,48 +1,78 @@
 import React, { Component } from 'react';
-import './App.css';
 import Note from './note/Note';
 import NoteForm from './noteForm/NoteForm';
-
 import { DB_CONFIG } from './Config/config';
 import firebase from 'firebase/app';
+import 'firebase/database';
+import './App.css';
 
 class App extends Component {
-  constructor(props) {
+
+  constructor(props){
     super(props);
+    this.addNote = this.addNote.bind(this);
+    this.removeNote = this.removeNote.bind(this);
 
     this.app = firebase.initializeApp(DB_CONFIG);
-    this.appdb = this.app.database().ref().child('notes');
-    
+    this.database = this.app.database().ref().child('notes');
+
+    // We're going to setup the React state of our component
     this.state = {
-      notes: [
-        { id: 1, noteContent: 'Nota 1 aqui!' },
-        { id: 2, noteContent: 'Nota 2 aqui!' },
-      ],
+      notes: [],
     }
   }
 
-
-
-  addNote = note => {
+  componentWillMount(){
     const previousNotes = this.state.notes;
-    previousNotes.push({ id: previousNotes.length + 1, noteContent: note });
-    this.setState({
-      notes: previousNotes,
-    });
+    // DataSnapshot
+    this.database.on('child_added', snap => {
+      previousNotes.push({
+        id: snap.key,
+        noteContent: snap.val().noteContent,
+      })
+
+      this.setState({
+        notes: previousNotes
+      })
+    })
+
+    this.database.on('child_removed', snap => {
+      for(var i=0; i < previousNotes.length; i++){
+        if(previousNotes[i].id === snap.key){
+          previousNotes.splice(i, 1);
+        }
+      }
+
+      this.setState({
+        notes: previousNotes
+      })
+    })
+  }
+
+  addNote(note){
+    this.database.push().set({ noteContent: note});
+  }
+
+  removeNote(noteId){
+    console.log("from the parent: " + noteId);
+    this.database.child(noteId).remove();
   }
 
   render() {
     return (
       <div className="notesWrapper">
         <div className="notesHeader">
-          <h1 className="heading">React and Firebase To-Do List</h1>
+          <div className="heading">React & Firebase To-Do List</div>
         </div>
         <div className="notesBody">
           {
-            this.state.notes.map(note => {
+            this.state.notes.map((note) => {
               return (
-                <Note noteContent={note.noteContent} noteId={note.noteId} key={note.id} />
-              );
+                <Note noteContent={note.noteContent} 
+                noteId={note.id} 
+                key={note.id} 
+                removeNote ={this.removeNote}/>
+              )
             })
           }
         </div>
